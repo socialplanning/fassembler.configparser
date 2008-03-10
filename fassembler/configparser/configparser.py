09cf179d88adbe5151d7b1c05ad02ca8077b106f
Ethan Jucovy <ethan.jucovy+github@gmail.com>
2008-03-10 20:53:58 +0000

    experimenting with a build.ini parser
    git-svn-id: https://svn.socialplanning.org/svn/fassembler/configparser@14309 40f73b84-6177-4a8e-8c61-7eee4b1974ed

---
+++ fassembler/configparser/configparser.py
@@ -0,0 +1,22 @@
+import os
+import ConfigParser
+
+def get_config(option):
+    env = os.environ['VIRTUAL_ENV']
+    env_name, env_path = (os.path.basename(env), os.path.dirname(env))
+
+    ini_path = os.sep.join([env_path, 'etc', 'build.ini'])
+
+    parser = ConfigParser.SafeConfigParser()
+
+    parser.read(ini_path)
+
+    real_option = ' '.join(env_name, option)
+    try:
+        return parser.get('applications', real_option)
+    except ConfigParser.NoOptionError:
+        pass
+    try:
+        return parser.get('general', option)
+    except ConfigParser.NoOptionError:
+        pass
